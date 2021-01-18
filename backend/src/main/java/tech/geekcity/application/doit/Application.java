fde1a3609a7c1b163b7566642bc9e66940e6945d
王智 <z.wang@tianrang-inc.com>
2021-01-18 16:08:47 +0800

    add springboot

--- backend/src/main/java/tech/geekcity/application/doit/Application.java
+++ backend/src/main/java/tech/geekcity/application/doit/Application.java
@@ -1,10 +1,29 @@
 package tech.geekcity.application.doit;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import java.io.File;
 import java.io.IOException;
 
-public class Application {
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
+
+    @Override
+    public void run(String... args) throws Exception {
+        LOGGER.info("running with arguments: {}", String.join(" ", args));
+        showTasks();
+    }
+
     public static void main(String[] args) throws IOException {
+        SpringApplication.run(Application.class, args);
+    }
+
+    private void showTasks() throws IOException {
         File dataFile = new File("backend/data/ben.wangz.data");
         TaskManager taskManager = TaskManager.Builder.newInstance()
                 .dataFile(dataFile)
